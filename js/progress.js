4ef594232658749106628efcd14dd69fdd9be751
Nalipi Sridhar Reddy <108881367+sridharreddy7831@users.noreply.github.com>
2024-08-03 16:24:39 +0530

    Add files via upload

---
+++ js/progress.js
@@ -0,0 +1,37 @@
+import { util } from './util.js';
+
+export const progress = (() => {
+
+    const assets = document.querySelectorAll('img');
+    const info = document.getElementById('progress-info');
+    const bar = document.getElementById('progress-bar');
+
+    const total = assets.length;
+    let loaded = 0;
+
+    // Minimum duration in milliseconds (e.g., 5 seconds)
+const minLoadingDuration = 5000; 
+let startTime = Date.now();
+let isMinDurationPassed = false;
+    
+
+    const progress = () => {
+        loaded += 1;
+
+        // bar.style.width = Math.min((loaded / total) * 100, 100).toString() + "%";
+        // info.innerText = `Loading assets (${loaded}/${total}) [${parseInt((loaded / total) * 100).toFixed(0)}%]`;
+
+        if (loaded == total) {
+            util.show();
+        }
+    };
+
+    info.style.display = 'block';
+    assets.forEach((asset) => {
+        if (asset.complete && asset.naturalWidth !== 0) {
+            progress();
+        } else {
+            asset.addEventListener('load', () => progress());
+        }
+    });
+})();
